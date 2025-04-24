5c024b0481148b259f5d37aa66e7fd6d102d7433
Alex <alexpertsi@gmail.com>
2025-04-24 06:07:21 -0400

    Function Closure and Function Hoisting - Develop Followers Count Milestone Alerts Using Functions

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -7,3 +7,13 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let grocery1, grocery2, grocery3;
+
+function groceryTracker() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+    let groceryTotal = grocery1 + grocery2 + grocery3;
+    document.getElementById('result').innerText = `The Total Amount is:$ ${groceryTotal}`;
+}
