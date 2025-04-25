94d50110a18b69fb24f1fdca87cf47f898e63a1e
Alex Pertsi <alexpertsi@gmail.com>
2025-04-25 12:07:01 +0300

    Update restaurant_menu.js

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -8,7 +8,9 @@ document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML
 let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
 mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
-document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;let dessertItem = '';
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
 for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
